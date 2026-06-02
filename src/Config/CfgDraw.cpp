8275cb95e7698c3a464449c0b07dcbfa92f8cec7
Codebuff <codebuff@codebuff.com>
2026-06-02 23:46:19 +0800

    清理CfgDraw.cpp：移除2个孤儿构造函数桩，修复Add签名

--- src/Config/CfgDraw.cpp
+++ src/Config/CfgDraw.cpp
@@ -3,38 +3,14 @@
 #include <cstring>
 #include <algorithm>
 
-CfgDraw::CfgDraw(int8_t type)
+void CfgDraw::Add(CfgDrawReward * reward)
 {
     // TODO: 从反编译代码还原实现
     // 反编译代码片段:
-    // void __cdecl CfgDraw::CfgDraw(int8_t type)
-    // {
-    // this->m_nType = type;
-    // this->m_nMaxRate = 0;
-    // std::list<CfgDrawReward>::list(&this->m_rewards);
-    // }
-}
-
-void CfgDraw::Add(const CfgDrawReward *const reward)
-{
-    // TODO: 从反编译代码还原实现
-    // 反编译代码片段:
-    // void __cdecl CfgDraw::Add(const CfgDrawReward *const reward
+    // void __cdecl CfgDraw::Add(CfgDrawReward * const reward
     // {
     // std::list<CfgDrawReward>::push_back(&this->m_rewards, reward);
     // this->m_nMaxRate += reward->nRate;
-    // }
-}
-
-CfgDraw::CfgDraw(const CfgDraw *const a2)
-{
-    // TODO: 从反编译代码还原实现
-    // 反编译代码片段:
-    // void __cdecl CfgDraw::CfgDraw(const CfgDraw *const a2)
-    // {
-    // this->m_nType = a2->m_nType;
-    // this->m_nMaxRate = a2->m_nMaxRate;
-    // std::list<CfgDrawReward>::list(&this->m_rewards, &a2->m_rewards);
     // }
 }
 
